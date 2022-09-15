cf2e7ed88d594b01187edafc61ffc292003a9722
Sam Nayrouz <samuel.nayrouz@gmail.com>
2022-09-15 10:17:49 -0600

    Create fetch func and enumarate results

--- 02-introduction-to-modern-concurrency/02-starter.playground/Contents.swift
+++ 02-introduction-to-modern-concurrency/02-starter.playground/Contents.swift
@@ -31,3 +31,23 @@
 /// THE SOFTWARE.
 
 import SwiftUI
+
+func fetchDomains() async throws -> [Domain] {
+    let url = URL(string: "https://api.raywanderlich.com/api/domains")!
+    let (data, _) = try await URLSession.shared.data(from: url)
+    
+    return try JSONDecoder().decode(Domains.self, from: data).data
+}
+
+Task {
+    do {
+        let domains = try await fetchDomains()
+        
+        for(index, domain) in domains.enumerated() {
+            let attr = domain.attributes
+            print("\(index + 1) \(attr.name): \(attr.description) - \(attr.level)")
+        }
+    } catch {
+        print(error)
+    }
+}
